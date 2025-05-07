f81d5f39de4ddd1be4d7f79593e511dbcb4ac4cb
Greshnik <sanmaximus92@mail.ru>
2025-05-07 12:33:31 +0300

    update CLI interface

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,15 +6,14 @@ import argparse
 def parse_args():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description="Generate diff",
+        description="Compares two configuration files and shows a difference.",
     )
-    parser.add_argument("first_file", help="path to first file")
-    parser.add_argument("second_file", help="path to second file")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
     parser.add_argument(
         "-f",
         "--format",
         help="set format of output",
-        default="stylish",
     )
     return parser.parse_args()
 
