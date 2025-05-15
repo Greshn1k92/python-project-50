fc1f149ab79f796d5bb2cf00239ac97ded6e812c
Greshnik <sanmaximus92@mail.ru>
2025-05-15 12:17:20 +0300

    Поправил импорт в init.py

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,5 +1,5 @@
 from gendiff.formatters.plain import format_diff as plain
-from gendiff.formatters.stylish import format_diff as stylish
+from gendiff.formatters.stylish import format_diff_stylish as stylish
 
 FORMATTERS = {
     'stylish': stylish,
