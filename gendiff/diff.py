52d86fa97bac811270e0c1768859b1bc04f10827
Greshnik <sanmaximus92@mail.ru>
2025-05-16 10:07:11 +0300

    feat: add parse_file function and update generate_diff to load data from files

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,16 +1,22 @@
 from typing import Any, Dict
-
 from gendiff.diff_tree import build_diff
 from gendiff.formatters import FORMATTERS
+from gendiff.parsers import parse_file
 
 
 def generate_diff(
-    data1: Dict[str, Any],
-    data2: Dict[str, Any],
+    file_path1: str,
+    file_path2: str,
     format_name: str = 'stylish'
 ) -> str:
+    
+    data1 = parse_file(file_path1)
+    data2 = parse_file(file_path2)
+    
+    
     diff = build_diff(data1, data2)
     
+
     if format_name not in FORMATTERS:
         raise ValueError(f"Unknown format: {format_name}")
     
---
+++ gendiff/parsers.py
@@ -0,0 +1,13 @@
+import json
+import yaml
+from typing import Dict, Any
+
+
+def parse_file(file_path: str) -> Dict[str, Any]:
+    with open(file_path) as f:
+        if file_path.endswith('.json'):
+            return json.load(f)
+        elif file_path.endswith(('.yml', '.yaml')):
+            return yaml.safe_load(f)
+        else:
+            raise ValueError(f"Unsupported file format: {file_path}") 
